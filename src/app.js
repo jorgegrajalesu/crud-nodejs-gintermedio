c4a48b0487df9ddb885af50d01cde88a1471d51f
jorgegrajalesu <jegosystem@gmail.com>
2023-11-01 19:56:03 -0500

    011123

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
+// importar los paquetes
+const express = require('express');
+const {engine} = require('express-handlebars');
+const myconnection = require('express-mysql-connection');
+const bodyParser = require('body-parser');
+const mysql      = require('mysql');
+
+
+
 console.log("crud");
